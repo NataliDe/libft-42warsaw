1ea0c3b7cb6fed5711cecb3627a491271a5c7307
Natalia Demkiv <129962650+NataliDe@users.noreply.github.com>
2025-10-01 23:28:13 +0200

    Update ft_memcmp.c

--- Libft/ft_memcmp.c
+++ Libft/ft_memcmp.c
@@ -14,16 +14,19 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char	*s1c;
-	unsigned char	*s2c;
-	size_t			i;
+	const unsigned char	*p1;
+	const unsigned char	*p2;
+	size_t				i;
 
-	i = -1;
-	s1c = (unsigned char *)s1;
-	s2c = (unsigned char *)s2;
-	while (++i < n && *(s1c + i) == *(s2c + i))
-		;
-	if (i == n)
-		return (0);
-	return (*(s1c + i) - *(s2c + i));
+	p1 = (const unsigned char *)s1;
+	p2 = (const unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (p1[i] != p2[i])
+			return (p1[i] - p2[i]);
+		i++;
+	}
+	return (0);
 }
+
