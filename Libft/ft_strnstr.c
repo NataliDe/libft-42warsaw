d2ae2f6486da85c1069fd77d0c40bf3e659cbed7
Natalia Demkiv <129962650+NataliDe@users.noreply.github.com>
2025-10-01 23:32:56 +0200

    Update ft_strnstr.c

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -15,16 +15,22 @@
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
+	size_t	j;
 
-	i = 0;
-	if (!*needle)
+	if (*needle == '\0')
 		return ((char *)haystack);
-	while (i < len && haystack[i])
+	i = 0;
+	while (haystack[i] && i < len)
 	{
-		if (ft_strncmp(haystack + i, needle, ft_strlen(needle)) == 0
-			&& (i + ft_strlen(needle) - 1) < len)
-			return ((char *)(haystack + i));
+		j = 0;
+		while (haystack[i + j] == needle[j] && (i + j) < len)
+		{
+			if (needle[j + 1] == '\0')
+				return ((char *)(haystack + i));
+			j++;
+		}
 		i++;
 	}
 	return (NULL);
 }
+
