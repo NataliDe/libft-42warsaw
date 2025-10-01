73c9a1a7ee415e2bd7038a5377b1e6a388d3f4d0
Natalia Demkiv <129962650+NataliDe@users.noreply.github.com>
2025-10-01 23:40:34 +0200

    Update ft_atoi.c

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -14,10 +14,10 @@
 
 int	ft_atoi(const char *str)
 {
-	long	res;
 	int		sign;
+	long	result;
 
-	res = 0;
+	result = 0;
 	sign = 1;
 	while (*str == ' ' || (*str >= 9 && *str <= 13))
 		str++;
@@ -29,12 +29,9 @@ int	ft_atoi(const char *str)
 	}
 	while (*str >= '0' && *str <= '9')
 	{
-		if (sign == 1 && (res > 214748364 || (res == 214748364 && *str - '0' > 7)))
-			return (2147483647);
-		if (sign == -1 && (res > 214748364 || (res == 214748364 && *str - '0' > 8)))
-			return (-2147483648);
-		res = res * 10 + (*str - '0');
+		result = result * 10 + (*str - '0');
 		str++;
 	}
-	return ((int)(res * sign));
+
+	return ((int)(result * sign));
 }
