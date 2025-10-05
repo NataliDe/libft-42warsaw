486b7859535aa6e89d426d44f6f38f40d7b50371
Natalia Demkiv <129962650+NataliDe@users.noreply.github.com>
2025-10-05 23:24:11 +0200

    Update ft_strjoin.c

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -14,25 +14,36 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
+	size_t	i;
+	size_t	j;
 	size_t	len1;
 	size_t	len2;
-	size_t	total;
 	char	*str;
 
 	if (!s1 || !s2)
 		return (NULL);
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	if (len1 > (size_t)-1 - len2 - 1)
-		return (NULL);
-	total = len1 + len2 + 1;
-
-	str = (char *)malloc(total);
+	len1 = 0;
+	while (s1[len1])
+		len1++;
+	len2 = 0;
+	while (s2[len2])
+		len2++;
+	str = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
 	if (!str)
 		return (NULL);
-
-	ft_memcpy(str, s1, len1);
-	ft_memcpy(str + len1, s2, len2);
-	str[total - 1] = '\0';
+	i = 0;
+	while (i < len1)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < len2)
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
 	return (str);
 }
+
